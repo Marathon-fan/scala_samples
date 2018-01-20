22f0af71e64ad3513cec58c0b86bf9236dd01876
John <johnprogrammer2005@gmail.com>
2018-01-20 08:09:55 +0800

    project 3 works- a new version

--- project3/spraydemo/src/main/scala/example/SprayDemo.scala
+++ project3/spraydemo/src/main/scala/example/SprayDemo.scala
@@ -7,14 +7,30 @@ import akka.actor.{Props, Actor, ActorSystem}
 object SprayDemo extends App with SimpleRoutingApp { //SimpleRoutingApp is production-level http server
   implicit val actorSystem = ActorSystem()
 
+  // type Route = RequestContext => Unit
+
   startServer(interface = "localhost", port = 8080) {
     get {
-      pathPrefix("takeaway" / "hello") {
+
+      //  curl "http://localhost:8080/takeaway/hello"
+      path ("takeaway" / "hello") {
           complete {
             "Welcome to the potato & steak take-away!"
           }
         }
+      } ~
+
+    //  curl "http://localhost:8080/order/potatoes?mashed=false&number=10&special=baked"
+    path("order" / "potatoes") {
+        parameters("mashed".as[Boolean], "number".as[Int], "special"?) {   // special is an optional parameter
+          (mashed, number, special) =>
+            complete {
+              s"You have orderd ${mashed} potatoes, ${number + 1} (one is free)" + s"Special wishes: ${special.getOrElse("none")}"
+            }
+
+        }
       }
+
   }
 
 }
