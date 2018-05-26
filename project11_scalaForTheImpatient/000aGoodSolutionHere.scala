ff126fcb30d00ebec347ee5fde4056d7f32a5a87
John Zhou <johnprogrammer2005@gmail.com>
2018-05-26 18:36:49 +1000

    advancement in project11_scalaForTheImpatient

--- project11_scalaForTheImpatient/000aGoodSolutionHere.scala
+++ project11_scalaForTheImpatient/000aGoodSolutionHere.scala
@@ -2,6 +2,10 @@
 
 tableOfContents.scala
 
-
+solution1:
 https://github.com/derlin/scala-for-the-impatient/blob/master/src/main/scala/chapter03.sc
+
+
+solution2:
+https://github.com/Gerhut/scala-for-the-impatient
 
